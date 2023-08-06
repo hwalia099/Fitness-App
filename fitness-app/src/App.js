83f879e0b279d9773680788289437fc9ca1db388
vigneshperumal2498 <perumalsamy.v@northeastern.edu>
2023-08-06 15:03:16 -0400

    Integrated Rapid Api for Chat GPT

--- fitness-app/src/App.js
+++ fitness-app/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import {Route, Routes} from 'react-router-dom';
-import {Box} from '@mui/material';
-import ExerciseDetail from './pages/ExerciseDetail';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Plan from "./pages/Plan";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
-    <Box width = "400px" sx={{width: { xl: '1488px'}}} m="auto">
-      <Navbar/>
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
       <Routes>
-        <Route path="/" element ={<Home/>}/>
-        <Route path="/exercise/:id" element ={<ExerciseDetail/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        <Route path="/plan" element={<Plan />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Box>
-  )
-}
+  );
+};
 
 export default App;
