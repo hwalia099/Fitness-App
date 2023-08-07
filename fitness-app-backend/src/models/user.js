0c1c1615e0439e60d859e38e5c3dabcacc95ffa5
hwalia099 <50072985+hwalia099@users.noreply.github.com>
2023-08-07 04:37:29 -0400

    user model updated

--- fitness-app-backend/src/models/user.js
+++ fitness-app-backend/src/models/user.js
@@ -1,33 +1,37 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-    userName: {
-        type: String,
-        required: true
-    },
-    userEmail: {
-        type: String,
-        required: true
-    },
-    userPhone: {
-        type: String,
-        required: true
-    },
-    createdDate: {
-      type: Date,
-      required: true,
-    },
-    modifiedDate: {
-      type: Date,
-      required: true,
-    },
-    isActive: {
-      type: Boolean,
-      required: true,
-    }
+  userName: {
+    type: String,
+    required: true
+  },
+  userEmail: {
+    type: String,
+    required: true
+  },
+  userPhone: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdDate: {
+    type: Date,
+    required: true,
+  },
+  modifiedDate: {
+    type: Date,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+  }
 
 }, {
-  versionKey: false 
+  versionKey: false
 });
 
 const User = mongoose.model('User', UserSchema);
