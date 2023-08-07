3e2e35f8703234b891fcd50e73e4bf000b328a08
hwalia099 <50072985+hwalia099@users.noreply.github.com>
2023-08-07 02:07:10 -0400

    signup and login form added

---
+++ fitness-app/src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+// src/pages/LoginPage.js
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h2>Log In</h2>
+            <LoginForm />
+        </div>
+    );
+};
+
+export default LoginPage;
