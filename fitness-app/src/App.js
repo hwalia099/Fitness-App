d96520d5a0f599100ee07ab8a60a4fb5395a2e9b
israniraksha <114438999+israniraksha@users.noreply.github.com>
2023-08-06 20:31:43 -0400

    Revert "added other navigation for plan, record and track progress"

--- fitness-app/src/App.js
+++ fitness-app/src/App.js
@@ -5,9 +5,6 @@ import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import PlanMealsandWorkout from './pages/PlanMealsandWorkout';
-import RecordDetails from './pages/RecordDetails';
-import TrackProgress from './pages/TrackProgress';
 
 import './App.css';
 
@@ -18,9 +15,6 @@ const App = () => {
       <Routes>
         <Route path="/" element ={<Home/>}/>
         <Route path="/exercise/:id" element ={<ExerciseDetail/>}/>
-        <Route path="/plan" element ={<PlanMealsandWorkout/>}/>
-        <Route path="/record" element ={<RecordDetails/>}/>
-        <Route path="/track" element ={<TrackProgress/>}/>
       </Routes>
       <Footer/>
     </Box>
